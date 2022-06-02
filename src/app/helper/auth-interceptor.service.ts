9ec71025ad790eacdd81877ee0b54ce831d1e891
Max2 <chigov@gmail.com>
2022-06-02 22:01:02 +0300

    token

--- src/app/helper/auth-interceptor.service.ts
+++ src/app/helper/auth-interceptor.service.ts
@@ -1,10 +1,32 @@
 import { Injectable } from '@angular/core';
+import {HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {TokenStorageService} from "../service/token-storage.service";
+import {Observable} from "rxjs";
+
+
+const TOKEN_HEADER_KEY = 'Authorization';
 
 // сервис, которій будет каждій раз подставлять токен
 @Injectable({
   providedIn: 'root'
 })
-export class AuthInterceptorService {
+//для подставления токена имплементируем
+export class AuthInterceptorService implements HttpInterceptor{
 
-  constructor() { }
+  //инжекция сервиса
+  constructor(private tokenService: TokenStorageService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let authRequest = req;
+    const token = this.tokenService.getToken();
+    if (token != null) {
+      //при каждом запросе подставляется в header
+      authRequest = req.clone({headers: req.headers.set(TOKEN_HEADER_KEY, token)});
+    }
+    return next.handle(authRequest);
+  }
 }
+
+export const authInterceptorProviders = [
+  {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}
+];
