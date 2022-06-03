2e9e7c2e07a5a9647f8dc372f8255d2071159c92
chigov2 <chigov@gmail.com>
2022-06-03 18:15:36 +0300

    5-09=intercept_03-40

--- src/app/helper/error-interceptor.service.ts
+++ src/app/helper/error-interceptor.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {TokenStorageService} from "../service/token-storage.service";
+import {NotificationService} from "../service/notification.service";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
-export class ErrorInterceptorService {
+export class ErrorInterceptorService implements HttpInterceptor{
 
-  constructor() { }
+  constructor(private  tokenService: TokenStorageService,
+              private notificationService: NotificationService) { }
+
+  // @ts-ignore
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
+
+  }
 }
