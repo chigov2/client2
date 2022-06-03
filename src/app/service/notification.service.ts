0b6c7ca9d8a7372179f5373072e6b574acf4f818
chigov2 <chigov@gmail.com>
2022-06-03 15:26:58 +0300

    5-07-image-service

--- src/app/service/notification.service.ts
+++ src/app/service/notification.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {config} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
 
-  constructor() { }
+  constructor(private snackBar: MatSnackBar) { }
+
+  public showSnackBar(message: string): void{
+    // @ts-ignoren
+    this.snackBar.open(message,null,{
+      duration:2000
+    });
+  }
 }
