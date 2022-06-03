22e939be3adf782afd7b182113a991e1024b2602
Max2 <chigov@gmail.com>
2022-06-03 21:15:00 +0300

    08-09

--- src/app/helper/error-interceptor.service.ts
+++ src/app/helper/error-interceptor.service.ts
@@ -1,19 +1,32 @@
 import { Injectable } from '@angular/core';
-import {HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {TokenStorageService} from "../service/token-storage.service";
-import {NotificationService} from "../service/notification.service";
-import {Observable} from "rxjs";
+import {HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {TokenStorageService} from '../service/token-storage.service';
+import {NotificationService} from '../service/notification.service';
+import {Observable, throwError} from 'rxjs';
+import {catchError} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ErrorInterceptorService implements HttpInterceptor{
+export class ErrorInterceptorService implements HttpInterceptor {
 
-  constructor(private  tokenService: TokenStorageService,
-              private notificationService: NotificationService) { }
+  constructor(private tokenService: TokenStorageService,
+              private notificationService: NotificationService) {
+  }
 
-  // @ts-ignore
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
-
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(catchError(err => {
+      if (err.status === 401) {
+        this.tokenService.logOut();
+        window.location.reload();
+      }
+      const error = err.error.message || err.statusText;
+      this.notificationService.showSnackBar(error);
+      return throwError(error);
+    }));
   }
 }
+
+export const authErrorInterceptorProviders = [
+  {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptorService, multi: true}
+];
